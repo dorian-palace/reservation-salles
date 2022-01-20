da1753d3b1ff7dfdaa269c7a25320a202940e081
dorian-palace <dorian.palace@laplateforme.io>
2022-01-20 16:34:38 +0100

    previousMonth & nextMonth finit

--- app/month.php
+++ app/month.php
@@ -1,49 +1,76 @@
 <?php
-require_once('bdd/bdd_log.php');
 class Month
 {
-
-    public $bdd;
     public $days = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
     private $months = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Décembre'];
-    private $month;
-    private $year;
+    public $month;
+    public $year;
 
     public function __construct(?int $month = null, ?int $year = null)
     {
-        //$this->bdd = Database::connexion_db();
-        if ($month === null) {
+        if ($month === null || $month < 1 || $month > 12) {
             $month = intval(date('m'));
         }
-        if ($year === null) {
+        if ($year === null ) {
             $year = intval(date('Y'));
         }
-        $month = $month % 12;
         $this->month = $month;
         $this->year = $year;
+        
     }
 
     //renvoi le premier jour du mois
-    public function getStartingDay(): \DateTime {
+    public function getStartingDay(): \DateTime
+    {
         return new \DateTime("{$this->year}-{$this->month}-01");
-
     }
 
 
     //Retourne le mois en string exe 07 = Juillet 
     public function toString(): string
     {
-        return  $this->months[$this->month - 1] . ' ' . $this->year;
+        return $this->months[$this->month - 1] . ' ' . $this->year;
     }
 
     public function getWeeks(): int
     {
+        // renvoi le nombre de semaine dans le mois
         $start = $this->getStartingDay();
         $end = (clone $start)->modify('+1 month -1 day');
-        $weeks = intval($end->format('W')) - intval($start->format(('W'))) +1;
+        $weeks = intval($end->format('W')) - intval($start->format( 'W')) + 1;
         if ($weeks < 0) {
             $weeks = intval($end->format('W'));
         }
         return $weeks;
+    }
+
+    public function withinMonth(\DateTime $date): bool
+    {
+        // Est-ce que le jour est dans le mois en cours
+        return $this->getStartingDay()->format('Y-m') === $date->format('Y-m'); //si le mois et l'année correspondent dans ce cas la les deux informations correspondent 
+    }
+
+    public function nextMonth(): Month
+    {
+        //renvoi le mois suivant
+        $month = $this->month + 1;
+        $year = $this->year;
+        if ($month > 12) {
+            $month = 1;
+            $year += 1;
+        }
+        return new Month($month, $year);
+    }
+
+    public function previousMonth(): Month
+    {
+        //renvoi le mois precedent
+        $month = $this->month - 1;
+        $year = $this->year;
+        if ($month < 1) {
+            $month = 12;
+            $year -= 1;
+        }
+        return new Month($month, $year);
     }
 }
--- planning.php
+++ planning.php
@@ -1,14 +1,7 @@
 <?php
 session_start();
 require('app/month.php');
-
-
-$month = new Month($_GET['month'] ?? null, $_GET['year'] ?? null);
-// ?? null Si c'est définit ça prend la valeur de $_GET sinon ça prend la valeur null
-$start = $month->getStartingDay()->modify('last monday');
 ?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -17,29 +10,46 @@ $start = $month->getStartingDay()->modify('last monday');
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/calendar.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>planning</title>
 </head>
 
 <body>
-    <?php include('element/header.php'); ?>
-    <main>
-        <h1 align="center">Planning</h1>
+    <?php
+    $month = new Month($_GET['month'] ?? null, $_GET['year'] ?? null);
+    // ?? null Si c'est définit ça prend la valeur de $_GET sinon ça prend la valeur null
+    $start = $month->getStartingDay()->modify('last monday');
+    // include('element/header.php'); 
+    ?>
+    
+    <div class="d-flex flex-row align-items-center justify-content-between mx-sm-3">
         <h1><?= $month->toString(); ?></h1>
+        <div>
+            <a href="planning.php?month=<?= $month->previousMonth()->month; ?>&year=<?= $month->previousMonth()->year; ?>" class="btn btn-primary">&lt;</a>
 
+            <a href="planning.php?month=<?= $month->nextMonth()->month; ?>&year=<?= $month->nextMonth()->year; ?>" class="btn btn-primary">&gt;</a>
 
-        <table class="calendar__table calendar__table--<?= $month->getWeeks(); ?>weeks">
-            <?php for ($i = 0; $i < $month->getWeeks(); $i++) : ?>
-                <tr>
-                    <?php foreach ($month->days as $k => $day) : ?>
-                        <td>
-                            <div class="calendar__weekday"><?= $day ?></div>
-                            <div class="calendar__day"><?= (clone $start)->modify("+$k days")->format('d'); ?></div>
-                        </td>
-                    <?php endforeach; ?>
-                </tr>
-            <?php endfor; ?>
-        </table>
-    </main>
+        </div>
+    </div>
+
+    <table class="calendar__table calendar__table--<?= $month->getWeeks(); ?>weeks">
+        <?php for ($i = 0; $i < $month->getWeeks(); $i++) : ?>
+            <tr>
+                <?php
+                foreach ($month->days as $k => $day) :
+                    $date = (clone $start)->modify("+" . ($k + $i * 7) . "days")
+                ?>
+                    <td class="<?= $month->withinMonth($date) ? '' : 'calendar__othermonth'
+                                //; 
+                                ?>">
+                        <?php if ($i === 0) : ?> <div class="calendar__weekday"><?= $day ?></div>
+                        <?php endif; ?>
+                        <div class="calendar__day"><?= $date->format('d'); ?></div>
+                    </td>
+                <?php endforeach; ?>
+            </tr>
+        <?php endfor; ?>
+    </table>
 
 </body>
 
